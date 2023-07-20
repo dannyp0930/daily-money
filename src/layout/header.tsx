ac5b08c20d9c544a3cea253a0720b781051613ca
dannyp0930 <dannyp0930@onoffmix.com>
2023-07-20 19:49:56 +0900

    초기 세팅

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,3 +1,12 @@
+import Image from "next/image";
+import Icon from "public/assets/icons/icon-512x512.png";
+import "@/styles/layout/header.css";
+
 export default function Header() {
-  return <div>헤더 입니다.</div>;
+  return (
+    <header>
+      <h1>123123</h1>
+      <Image src={Icon} width={20} height={20} alt="daily_money" />
+    </header>
+  );
 }
