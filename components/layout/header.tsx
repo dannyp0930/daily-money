ff258dce78afc1afa26cfaae9d9dbef3158b099d
dannyp0930 <dannyp0930@onoffmix.com>
2023-08-06 17:39:31 +0900

    구조 변경

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -5,7 +5,6 @@ import "@/styles/layout/header.css";
 export default function Header() {
   return (
     <header>
-      <h1>123123</h1>
       <Image src={Icon} width={20} height={20} alt="daily_money" />
     </header>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
     <main>
+      <h1>Daily Money</h1>
     </main>
   )
 }
